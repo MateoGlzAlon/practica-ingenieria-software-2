f928c3ce0a76bbe1bf2da4a3c9850a3e1ccc04ca
vacevh00 <72200146+vacevh00@users.noreply.github.com>
2025-04-15 15:57:10 +0200

    feat: endpoints search PostByID and CommentByPostID done

--- backend/src/main/java/com/backend/controller/PostController.java
+++ backend/src/main/java/com/backend/controller/PostController.java
@@ -3,6 +3,7 @@ package com.backend.controller;
 import com.backend.persistence.entity.PostEntity;
 import com.backend.persistence.specialdto.FeedDTO;
 import com.backend.persistence.specialdto.FeedPostDTO;
+import com.backend.persistence.inputDTO.PostInputDTO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +12,15 @@ import java.util.List;
 
 @RequestMapping("/posts")
 public interface PostController {
-
+    
+    //merge this one and the "getPostIndividual()" if its not used
     @GetMapping("/{id}")
     PostEntity findPostById(@PathVariable Long id);
 
     @GetMapping("/landingPageFeed")
     List<FeedPostDTO> getFeedPosts();
 
+    @GetMapping("/focus/{id}")
+    PostInputDTO getPostIndividual(@PathVariable Long id);
 
 }
--- backend/src/main/java/com/backend/controller/impl/CommentControllerImpl.java
+++ backend/src/main/java/com/backend/controller/impl/CommentControllerImpl.java
@@ -3,12 +3,14 @@ package com.backend.controller.impl;
 import com.backend.controller.CommentController;
 import com.backend.persistence.entity.CommentEntity;
 import com.backend.service.CommentService;
+import com.backend.persistence.inputDTO.CommentInputDTO;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 
 @RestController
 @RequestMapping("/comments")
@@ -21,6 +23,12 @@ public class CommentControllerImpl implements CommentController {
     @GetMapping("/{id}")
     public CommentEntity findCommentById(@PathVariable Long id) {
         return commentService.findCommentById(id);
+    }
+
+    @Override
+    @GetMapping("/post/{id}")
+    public List<CommentInputDTO> findCommentsOfAPost(@PathVariable Long id){
+        return commentService.findCommentsOfAPost(id);
     }
 }
 
