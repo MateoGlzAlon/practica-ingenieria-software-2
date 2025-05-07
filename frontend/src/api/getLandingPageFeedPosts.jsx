7bb3e2756bb6cb1f31bfeab47e92520b776f3c82
MateoGlzAlon <mateo.gonzalez.alonso@gmail.com>
2025-05-07 17:12:51 +0200

    fix: axios argument

--- frontend/src/api/getLandingPageFeedPosts.jsx
+++ frontend/src/api/getLandingPageFeedPosts.jsx
@@ -1,13 +1,12 @@
 import axios from 'axios';
 import { DATA } from "@/app/data"
 
-export default async function getFeedPosts() {
+export default async function getFeedPosts(page = 0, size = 10) {
     try {
         const response = await axios.get(`${DATA.apiURL}/posts/landingPageFeed`, {
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
+            params: { page, size },
+            headers: { 'Content-Type': 'application/json' },
+        })
 
         return response.data;
     } catch (error) {
