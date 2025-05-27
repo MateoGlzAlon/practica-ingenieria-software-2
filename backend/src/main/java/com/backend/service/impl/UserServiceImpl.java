47c890a163c0ce4e1e57538664892cca8812e197
MateoGonzalezz <mateo.gonzalez.alonso@gmail.com>
2025-05-27 23:42:33 +0200

    test: 100% coverage on unitary service tests

--- backend/src/main/java/com/backend/service/impl/UserServiceImpl.java
+++ backend/src/main/java/com/backend/service/impl/UserServiceImpl.java
@@ -95,6 +95,8 @@ public class UserServiceImpl implements UserService {
         );
 
 
+        if (userData == null) return null;
+
         UserOutputDTO userDto = UserOutputDTO.builder()
                 .id(userData.getId())
                 .name(userData.getName())
