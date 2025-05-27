44c47e424664bee24bf0f746dbd05eefb6b42c80
MateoGonzalezz <mateo.gonzalez.alonso@gmail.com>
2025-05-27 23:12:42 +0200

    fixed login and navbar profile

--- frontend/src/hooks/getUserIdAuth.jsx
+++ frontend/src/hooks/getUserIdAuth.jsx
@@ -1,30 +1,14 @@
 import axios from 'axios';
-import jwtDecode from 'jwt-decode';
+import { jwtDecode } from 'jwt-decode';
 
-const getUserIdAuth = async () => {
-  try {
 
-    const token = localStorage.getItem('token');
-    if (!token) {
-        //error, couldnt find token
-        return;
-    }
+export default function getUserIdFromLocalStorage() {
 
-    const decoded = jwtDecode(token);
-    const email = decoded.email;
-
-    const response = await axios.get('http://localhost:8080/users/getId', {
-        params: { email }
-    });
-
-    const userId = response.data;
-    localStorage.setItem('userId', userId);
-    
-    
-
-  } catch (error) {
-    console.error('ERROR could not retreive the email:', error);
+  if (typeof window !== 'undefined' && localStorage) {
+    console.log('getUserIdAuth4', localStorage.getItem('userId'));
+    return localStorage.getItem('userId');
   }
+  return null;
 };
 
-export default getUserIdAuth;
+
