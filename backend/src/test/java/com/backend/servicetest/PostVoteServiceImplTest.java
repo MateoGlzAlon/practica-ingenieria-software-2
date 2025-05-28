18f45a0ea7978c65ed57a008881795383c020628
yaloncu <yaizaalonsocuervo@gmail.com>
2025-05-28 16:46:41 +0200

    PostVoteServiceImplTest deletions sonar

--- backend/src/test/java/com/backend/servicetest/PostVoteServiceImplTest.java
+++ backend/src/test/java/com/backend/servicetest/PostVoteServiceImplTest.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-public class PostVoteServiceImplTest {
+class PostVoteServiceImplTest {
 
     @Mock
     private PostVoteRepository postVoteRepository;
@@ -25,7 +25,7 @@ public class PostVoteServiceImplTest {
     private PostVoteEntity mockVote;
 
     @BeforeEach
-    public void setup() {
+    void setup() {
         MockitoAnnotations.openMocks(this);
         mockVote = PostVoteEntity.builder()
                 .id(1L)
@@ -35,7 +35,7 @@ public class PostVoteServiceImplTest {
     }
 
     @Test
-    public void testFindPostById_ReturnsPostVote() {
+    void testFindPostById_ReturnsPostVote() {
         when(postVoteRepository.findById(1L)).thenReturn(Optional.of(mockVote));
 
         PostVoteEntity result = postVoteService.findPostVoteById(1L);
@@ -46,7 +46,7 @@ public class PostVoteServiceImplTest {
     }
 
     @Test
-    public void testFindPostById_ReturnsNullWhenNotFound() {
+    void testFindPostById_ReturnsNullWhenNotFound() {
         when(postVoteRepository.findById(2L)).thenReturn(Optional.empty());
 
         PostVoteEntity result = postVoteService.findPostVoteById(2L);
