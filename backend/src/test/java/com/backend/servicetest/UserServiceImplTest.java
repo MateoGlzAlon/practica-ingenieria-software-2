5ba348cec82ed67cd01e33f2c96f1acdc6775a5b
yaloncu <yaizaalonsocuervo@gmail.com>
2025-05-28 16:50:52 +0200

    UserServiceImplTest deletions sonar

--- backend/src/test/java/com/backend/servicetest/UserServiceImplTest.java
+++ backend/src/test/java/com/backend/servicetest/UserServiceImplTest.java
@@ -7,7 +7,6 @@ import com.backend.persistence.specialdto.ProfileDTO;
 import com.backend.repository.*;
 import com.backend.service.impl.UserServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
