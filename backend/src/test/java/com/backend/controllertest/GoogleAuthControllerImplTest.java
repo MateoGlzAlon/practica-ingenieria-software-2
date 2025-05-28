02edfee290614a21c44455b4a55ad86775a3287e
yaloncu <yaizaalonsocuervo@gmail.com>
2025-05-28 16:22:18 +0200

    GoogleAuthControllerImplTest deletions sonar

--- backend/src/test/java/com/backend/controllertest/GoogleAuthControllerImplTest.java
+++ backend/src/test/java/com/backend/controllertest/GoogleAuthControllerImplTest.java
@@ -21,7 +21,6 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.http.ResponseEntity;
 
 import com.backend.controller.impl.GoogleAuthControllerImpl;
-import com.backend.controller.impl.PostControllerImpl;
 import com.backend.persistence.entity.PostEntity;
 import com.backend.persistence.entity.PostImageEntity;
 import com.backend.persistence.entity.TagEntity;
@@ -38,45 +37,25 @@ import com.backend.persistence.specialdto.FeedPostDTO;
 import com.backend.persistence.specialdto.PostDetailsDTO;
 import com.backend.persistence.specialdto.ProfileDTO;
 import com.backend.service.GoogleAuthService;
-import com.backend.service.PostService;
 
-public class GoogleAuthControllerImplTest {
+class GoogleAuthControllerImplTest {
     @Mock
     private GoogleAuthService googleAuthService;
 
     @InjectMocks
     private GoogleAuthControllerImpl googleAuthController;
 
-    private PostInputDTO mockPostInput;
     private UserEntity mockUserEntity;
     private TagEntity mockTagEntity;
-    private TagOutputDTO mockTagOutputDTO;
     private PostEntity mockPostEntity;
     private PostImageEntity mockPostImageEntity;
-    private TipEntity mockTipEntity;
-    private UserInputDTO mockUserInputDto;
-    private ProfileDTO mockProfileDto;
     private UserOutputDTO mockUserOutputDto;
-    private CommunityStatsDTO mockStats;
-    private PostImageEntity mockImage;
-    private PostEntity mockPost;
-    private PostOutputDTO mockPostOutput;
-    private PostDetailsDTO mockPostDetails;
-    private FeedPostDTO mockFeedPost;
     private GoogleLoginDTO mockLoginDTO;
 
 
     @BeforeEach
     public void setup() {
         MockitoAnnotations.openMocks(this);
-
-        mockPostInput = PostInputDTO.builder()
-                .title("Test Title")
-                .content("Test Content")
-                .tagId(1L)
-                .userId(1L)
-                .imageLinks(Arrays.asList("https://placehold.co/600x400?text=Post90"))
-                .build();
 
         mockUserEntity = UserEntity.builder()
                 .id(1L)
@@ -89,10 +68,6 @@ public class GoogleAuthControllerImplTest {
                 .id(1L)
                 .name("Java")
                 .posts(new HashSet<>())
-                .build();
-
-        mockTagOutputDTO = TagOutputDTO.builder()
-                .tags(List.of("mockTag"))
                 .build();
 
         mockPostImageEntity = PostImageEntity.builder()
@@ -119,64 +94,12 @@ public class GoogleAuthControllerImplTest {
                 .title("Post Title")
                 .build();
 
-        mockTipEntity = TipEntity.builder()
-                .id(1L)
-                .amount(100)
-                .post(post)
-                .createdAt(new Date())
-                .build();
-
-        
-        mockUserInputDto = UserInputDTO.builder()
-                .username("testuser")
-                .email("test@example.com")
-                .password("password")
-                .about("about user")
-                .build();
-
         mockUserOutputDto = UserOutputDTO.builder()
                 .id(1L)
                 .username("testuser")
                 .email("test@example.com")
                 .role("USER")
                 .about("about user")
-                .build();
-        mockProfileDto = ProfileDTO.builder()
-                .user(mockUserOutputDto)
-                .build();
-
-        mockStats = CommunityStatsDTO.builder()
-                .users(50L)
-                .questions(200L)
-                .answers (300L)
-                .build();
-
-        mockImage = PostImageEntity.builder()
-                .id(1L)
-                .imageUrl("https://placehold.co/600x400?text=PostImage")
-                .build();
-
-        mockPostInput = PostInputDTO.builder()
-                .title("Mock Post")
-                .content("Content")
-                .tagId(1L)
-                .userId(1L)
-                .build();
-
-        mockPostOutput = PostOutputDTO.builder()
-                .id(1L)
-                .title("Mock Post")
-                .content("Content")
-                .build();
-
-        mockPostDetails = PostDetailsDTO.builder()
-                .id(1L)
-                .title("Detailed Post")
-                .build();
-
-        mockFeedPost = FeedPostDTO.builder()
-                .id(1L)
-                .title("Feed Post")
                 .build();
 
         mockLoginDTO = GoogleLoginDTO.builder()
